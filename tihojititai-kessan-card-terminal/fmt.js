eb09818dceeb3d9f20720363694516c22ab69002
zaisei-kakushin-kai <zaisei-kakushin-kai@users.noreply.github.com>
2026-03-10 06:51:29 +0900

    create fmt.js

---
+++ tihojititai-kessan-card-terminal/fmt.js
@@ -0,0 +1,33 @@
+function fmt1000ennToKansuji(n) {
+    if (Number.isFinite(n)) {
+        const OKU = 10000
+        const CHO = 10000 * 10000
+        if (n >= OKU) {
+            // oku
+            const locales = new Intl.Locale("ja")
+            return `${(n / OKU).toLocaleString(locales, {
+                maximumFractionDigits: 1
+            })} 億円`
+        } else {
+            return `${n.toLocaleString()} 千円`
+        }
+    }
+    return "-"
+
+}
+
+function fmtPercentage(ratio) {
+    if (Number.isFinite(ratio)) {
+        return `${ratio.toFixed(2)} %`
+    } else {
+        return "-"
+    }
+}
+
+function fmtNumber(n) {
+    if (Number.isFinite(n)) {
+        return n
+    } else {
+        return "-"
+    }
+}
